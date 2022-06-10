b35fcf5aef39093d0be81061b500ebd1a074691e
Abraham Segura <abrahamdsm299@gmail.com>
2022-06-10 17:39:19 -0400

    add functins

---
+++ js/index.js
@@ -0,0 +1,53 @@
+import { createButton, change } from "./buttons.js";
+
+const result = document.querySelector('.result')
+const form = document.getElementById('form')
+const buttonContent = document.querySelector('.button-content');
+const btnEarth = document.querySelector('.btn-earth');
+const inputForm = document.getElementById('input-form')
+class Gravedad {
+    constructor(p, a) {
+        this.planeta = p;
+        this.aceleracion = a;
+    }
+}
+let gravedad = [];
+gravedad.push(new Gravedad('Mercurio', 3.7));
+gravedad.push(new Gravedad('Venus', 8.87));
+gravedad.push(new Gravedad('Luna', 1.62));
+gravedad.push(new Gravedad('Marte', 3.4));
+gravedad.push(new Gravedad('Jupiter', 24.8));
+gravedad.push(new Gravedad('Saturno', 10.44));
+gravedad.push(new Gravedad('Urano', 8.87));
+gravedad.push(new Gravedad('Neptuno', 11.15));
+gravedad.push(new Gravedad('Pluton', 0.67));
+
+addEventListener('DOMContentLoaded', () => {
+    let fragment = document.createDocumentFragment()
+    for (let i = 0; i < gravedad.length; i++) {
+        const btn = createButton(gravedad[i].planeta)
+        fragment.appendChild(btn)
+    }
+    buttonContent.appendChild(fragment)
+    //---------------------------------------------
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const Peso = form['input-form'].value;
+        const submiter = e.submitter.id;
+        if (Peso <= 0) {
+            return result.innerHTML = "Introduce un peso valido"
+        }
+        result.innerHTML = `Tu peso en ${submiter} es: ${change(peso, submiter, gravedad)}Kg`;
+    })
+    //---------------------------------------------
+    btnEarth.addEventListener('click', (e) => {
+        const Peso = form['input-form'].value;
+
+        if (Peso <= 0) {
+            return result.innerHTML = "Introduce un peso valido"
+        }
+        result.innerHTML = `Tu peso en La Tierra es: ${Peso}Kg`;
+    })
+})
+//---------------------------------------------
