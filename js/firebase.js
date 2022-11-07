d6438b1010fbba0cfec00d07fbf149b61af1ebd9
Abraham Segura <abrahamdsm299@gmail.com>
2022-11-07 13:02:06 -0400

    remove ;

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js"
 
-//import { getFirestore,getDatabase, ref, onValue} from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-SERVICE.js';
- 
-  const firebaseConfig = {
+//import { getFirestore,getDatabase, ref, onValue} from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-SERVICE.js'
+
+const firebaseConfig = {
   apiKey: "AIzaSyAM_Of1aOwGWuKFQUB_r0AjYit2SxSZ538",
   authDomain: "calculadora-planetaria.firebaseapp.com",
   projectId: "calculadora-planetaria",
   storageBucket: "calculadora-planetaria.appspot.com",
   messagingSenderId: "819618040077",
   appId: "1:819618040077:web:ce100944c084ed159a0844"
-};
-const app = initializeApp(firebaseConfig);
+}
+const app = initializeApp(firebaseConfig)
--- js/index.js
+++ js/index.js
@@ -1,71 +1,76 @@
+//--------------herpers-------------------------
 const $ = select => document.querySelector(select)
-
+//--------------elements------------------------
 const result = $('.result')
 const form = $('#form')
-const buttonContent = $('.button-content');
-const btnEarth = $('.btn-earth');
+const buttonContent = $('.button-content')
+const btnEarth = $('.btn-earth')
 const inputForm = $('#input-form')
+//----------------class-------------------------
 class Gravedad {
     constructor(p, a) {
-        this.planeta = p;
-        this.aceleracion = a;
+        this.planeta = p
+        this.aceleracion = a
     }
 }
-let gravedad = [];
-gravedad.push(new Gravedad('Mercurio', 3.7));
-gravedad.push(new Gravedad('Venus', 8.87));
-gravedad.push(new Gravedad('Luna', 1.62));
-gravedad.push(new Gravedad('Marte', 3.4));
-gravedad.push(new Gravedad('Jupiter', 24.8));
-gravedad.push(new Gravedad('Saturno', 10.44));
-gravedad.push(new Gravedad('Urano', 8.87));
-gravedad.push(new Gravedad('Neptuno', 11.15));
-gravedad.push(new Gravedad('Pluton', 0.67));
+let gravedad = []
+gravedad.push(new Gravedad('Mercurio', 3.7))
+gravedad.push(new Gravedad('Venus', 8.87))
+gravedad.push(new Gravedad('Luna', 1.62))
+gravedad.push(new Gravedad('Marte', 3.4))
+gravedad.push(new Gravedad('Jupiter', 24.8))
+gravedad.push(new Gravedad('Saturno', 10.44))
+gravedad.push(new Gravedad('Urano', 8.87))
+gravedad.push(new Gravedad('Neptuno', 11.15))
+gravedad.push(new Gravedad('Pluton', 0.67))
 
+//------------------events-----------------
 addEventListener('DOMContentLoaded', () => {
     let fragment = document.createDocumentFragment()
     gravedad.map(({ planeta }) => fragment.appendChild(createButton(planeta)))
 
-    // for (let i = 0; i < gravedad.length; i++) {
+    // for (let i = 0 i < gravedad.length i++) {
     //     const btn = createButton(gravedad[i].planeta)
     //     fragment.appendChild(btn)
     // }
     buttonContent.appendChild(fragment)
-    //---------------------------------------------
-    form.addEventListener('submit', e => {
-        e.preventDefault();
 
-        const Peso = form['input-form'].value;
-        const submiter = e.submitter.id;
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        const Peso = form['input-form'].value
+        const submiter = e.submitter.id
         if (Peso <= 0) {
             return result.innerHTML = "Introduce un peso valido"
         }
-        result.innerHTML = `Tu peso en ${submiter} es: ${change(Peso, submiter, gravedad)}Kg`;
+        result.innerHTML = `Tu peso en ${submiter} es: ${change(Peso, submiter, gravedad)}Kg`
     })
+
+
     //---------------------------------------------
     btnEarth.addEventListener('click', (e) => {
-        const Peso = form['input-form'].value;
+        const Peso = form['input-form'].value
 
         if (Peso <= 0) {
             return result.innerHTML = "Introduce un peso valido"
         }
-        result.innerHTML = `Tu peso en La Tierra es: ${Peso}Kg`;
+        result.innerHTML = `Tu peso en La Tierra es: ${Peso}Kg`
     })
 })
 //---------------------------------------------
 function createButton(id) {
-    const btn = document.createElement('button');
-    const planeta = id;
-    const text = document.createTextNode(`${planeta}`);
-    btn.id = planeta;
-    btn.className = 'btn';
-    btn.appendChild(text);
-    return btn;
+    const btn = document.createElement('button')
+    const planeta = id
+    const text = document.createTextNode(`${planeta}`)
+    btn.id = planeta
+    btn.className = 'btn'
+    btn.appendChild(text)
+    return btn
 }
 function change(p, id, listId) {
-    const earth = 9.8;
-    const planet = listId.filter(x => x.planeta == id);
-    const planetAcc = planet[0].aceleracion;
-    const change = Math.round(parseFloat((p / earth) * planetAcc));
-    return change;
+    const earth = 9.8
+    const planet = listId.filter(x => x.planeta === id)
+    const planetAcc = planet[0].aceleracion
+    const change = parseFloat((p / earth) * planetAcc)
+    return change
 }
